a7d8f9fcc98b4c8f740b7b9f3eec113487c7104b
Trevor Werner <trezrevner@icloud.com>
2023-08-28 20:43:25 -0400

    Lifted state up to main page, passing down to calendar

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -13,27 +13,13 @@ import {
 } from "../utils/dateHelpers"
 import Week from "../components/Week"
 
-export default function Calendar() {
-   const [dates, setDates] = useState([])
-   const [currentMonth, setCurrentMonth] =
-      useState(0)
-   const [currentYear, setCurrentYear] =
-      useState(0)
-   const [dataReady, setDataReady] =
-      useState(false)
-
-   useEffect(() => {
-      const today = new Date()
-      const monthIdx = today.getMonth()
-      const year = today.getFullYear()
-      const currentMonthDates =
-         generateMonthDates(monthIdx, year, today)
-      setCurrentMonth(months[monthIdx])
-      setCurrentYear(year)
-      setDates(currentMonthDates)
-      setDataReady(true)
-   }, [])
-
+export default function Calendar({
+   dates,
+   currentMonth,
+   currentYear,
+   dataReady
+}) {
+   
    return (
       <Paper
          elevation={3}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,41 @@
+"use client"
+import { useEffect, useState } from "react"
+import {
+   generateMonthDates,
+   months,
+   weekDays
+} from "./utils/dateHelpers"
 import Calendar from "./components/Calendar"
 
 export default function page() {
+   const [dates, setDates] = useState([])
+   const [currentMonth, setCurrentMonth] =
+      useState(0)
+   const [currentYear, setCurrentYear] =
+      useState(0)
+   const [dataReady, setDataReady] =
+      useState(false)
+
+   useEffect(() => {
+      const today = new Date()
+      const monthIdx = today.getMonth()
+      const year = today.getFullYear()
+      const currentMonthDates =
+         generateMonthDates(monthIdx, year, today)
+      setCurrentMonth(months[monthIdx])
+      setCurrentYear(year)
+      setDates(currentMonthDates)
+      setDataReady(true)
+   }, [])
+   
    return (
       <main>
-         <Calendar />
+         <Calendar
+            dates={dates}
+            currentMonth={currentMonth}
+            currentYear={currentYear}
+            dataReady={dataReady}
+         />
       </main>
    )
 }
