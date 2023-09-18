a56e6c4f9cbee38dc4de20095d3e0acfeeee2f9f
Trevor Werner <trezrevner@icloud.com>
2023-09-17 22:28:05 -0400

    Added data generation for calendar dates from 2015 - 2040

--- prisma/index.js
+++ prisma/index.js
@@ -1,10 +1,37 @@
 const { PrismaClient } = require('@prisma/client')
+const { generateMonthDates } = require('../app/utils/dateHelpers')
 
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+    // Generate date objects from 2015 - 2040
+    const today = new Date();
+    const startYear = 2015;
+    const endYear = 2040;
+
+    for (let year = startYear; year < endYear; year++) {
+        const monthsData = [];
+
+        for (let month = 0; month < 12; month++) {
+            monthsData.push({
+                month,
+                dates: {
+                    create: generateMonthDates(month, year, today),
+                },
+            });
+        }
+
+        await prisma.year.create({
+            data: {
+                year,
+                months: {
+                    create: monthsData,
+                },
+            },
+        });
+    }
 }
+
 
 main()
   .then(async () => {
