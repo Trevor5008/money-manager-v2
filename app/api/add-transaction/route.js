2696e6c8a89d979b5d8786d49560202d16666195
Trevor Werner <trezrevner@icloud.com>
2023-10-11 05:21:57 -0400

    Refactored transaction post route into separate methods

--- app/api/add-transaction/route.js
+++ app/api/add-transaction/route.js
@@ -2,50 +2,66 @@ const prisma = new PrismaClient()
 import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
 
-export async function POST(request){
-    const payload = await request.json()
-    const { category, year, month, date, account, itemType, amount } = payload.itemObj
-    
-
-    // Gather correct year id
-    const yearData = await prisma.year.findFirst({
-        where: {
-            year
-        },
-        select: {
+// Helper method to retrieve date id
+async function getDateId(year, month, date) {
+   // Retrieve year id
+   const { id: yearId } = await prisma.year.findFirst({
+      where: {
+         year
+      },
+      select: {
+         id: true
+      }
+   })
+   // Retrieve month id
+   const { id: monthId } = await prisma.month.findFirst(
+      {
+         where: {
+            month,
+            yearId
+         },
+         select: {
             id: true
-        }
-    })
+         }
+      }
+   )
+   // Retrieve date id  
+   const { id } = await prisma.date.findFirst({
+      where: {
+         date,
+         monthId
+      },
+      select: {
+         id: true
+      }
+   })
 
-    // Gather correct month id
-    const monthData = await prisma.month.findFirst({
-        where: {
-            yearId: yearData.id,
-            month
-        },
-        select: {
-            id: true
-        }
-    })
-    
-    // Gather correct date id
-    const dateData = await prisma.date.findFirst({
-        where: {
-            monthId: monthData.id,
-            date
-        },
-        select: { id: true }
-    })
-    
-    // TODO: Based on date id... add transaction (correct type)
-    await prisma.expense.create({
-        data: {
-            amount,
-            accountId: 1,
-            category,
-            isRecurring: false,
-            dateId: dateData.id
-        }
-    })   
-    // return NextResponse.json({dates})
+   return id
+}
+
+export async function POST(request) {
+   const payload = await request.json()
+   const {
+      category,
+      year,
+      month,
+      date,
+      account,
+      itemType,
+      amount
+   } = payload.itemObj
+
+   const dateId = await getDateId(year, month, date);
+
+   // TODO: Based on date id... add transaction (correct type)
+   await prisma.expense.create({
+      data: {
+         amount,
+         accountId: 1,
+         category,
+         isRecurring: false,
+         dateId
+      }
+   })
+   // return NextResponse.json({dates})
 }
