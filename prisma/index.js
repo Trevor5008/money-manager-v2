7e286f83a7fba9434c503346ec95999c9e9d18f1
Trevor Werner <trezrevner@icloud.com>
2023-11-01 00:03:48 -0400

    Fixed off-by-one error with month data

--- prisma/index.js
+++ prisma/index.js
@@ -12,11 +12,11 @@ async function main() {
     for (let year = startYear; year < endYear; year++) {
         const monthsData = [];
 
-        for (let month = 0; month < 12; month++) {
+        for (let month = 1; month <= 12; month++) {
             monthsData.push({
                 month,
                 dates: {
-                    create: generateMonthDates(month, year, today),
+                    create: generateMonthDates(month, year),
                 },
             });
         }
